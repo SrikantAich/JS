d2b4a1ba4211b0db7e15d498ffad15833a9d6f3d
srikant.12111982@lpu.in <srikant.12111982@lpu.in>
2025-05-06 08:15:19 +0530

    Varibles

--- Basics/Hello_world.js
+++ Basics/Hello_world.js
@@ -1,1 +1,7 @@
 console.log("Hello, World!");
+// console.log is a method of the global 'console' object, used to print messages to the debugging console.
+// The 'console' object provides various methods like log(), error(), warn(), and info() for different types of output.
+// It is available in most JavaScript environments, including web browsers and Node.js.
+// While the basic functionality is consistent, the way messages are displayed may vary slightly across different environments.
+// In web browsers, messages are typically displayed in the developer console, which can be accessed through the browser's developer tools.
+// In Node.js, messages are printed to the terminal or command prompt where the script is executed.
---
+++ Basics/variable.js
@@ -0,0 +1,12 @@
+//variables and constants and their declaration
+// Variables are containers for storing data values
+// In JavaScript, there are 3 ways to declare variables: var, let, and const
+// var is function scoped or globally scoped, let and const are block scoped
+// var can be re-declared and updated, let can be updated but not re-declared, const cannot be updated or re-declared
+// const is used to declare constants, which are variables that cannot be reassigned
+//block scope means that the variable is only accessible within the block of code where it is defined
+//function scope means that the variable is accessible within the function where it is defined
+//global scope means that the variable is accessible throughout the entire program
+
+
+
