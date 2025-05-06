7e8dbab4c2961dd8f0e0c037575ca95c137c43b4
srikant.12111982@lpu.in <srikant.12111982@lpu.in>
2025-05-06 12:17:29 +0530

    Added Objects

---
+++ Basics/DataTypes/Objects.js
@@ -0,0 +1,10 @@
+// 1.8 Object
+// A collection of key-value pairs — used to group related data
+let user = {
+    name: "Alice",
+    age: 25,
+    isAdmin: false
+  };
+
+console.log(user.name)
+console.log(user['age', 'isAdmin'])
