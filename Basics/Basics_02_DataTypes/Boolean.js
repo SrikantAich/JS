d910d610c593b7f415fca60861ea55f5b983e1ec
srikant.12111982@lpu.in <srikant.12111982@lpu.in>
2025-05-11 20:30:31 +0530

    Refactor Boolean.js for clarity and consistency

--- Basics/Basics_02_DataTypes/Boolean.js
+++ Basics/Basics_02_DataTypes/Boolean.js
@@ -1,9 +1,9 @@
 // 1.3 Boolean
 // Represents true/false — used in logic, conditions, toggles, validations.
-let bool1=true;
-let bool2=false;
-let bool3=Boolean(1);
-let bool4=Boolean(0);
+let bool1=true
+let bool2=false
+let bool3=Boolean(1)
+let bool4=Boolean(0)
 let bool5=Boolean('Hello')  //if you pass astring its true
 let bool6=Boolean('')       //if you pass an empty string its false
 let bool7=Boolean()         // this is also false
