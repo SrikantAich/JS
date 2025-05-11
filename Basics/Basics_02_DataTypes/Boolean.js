6ab48b56d780c6b4b995d86c3e0c927e70945926
srikant.12111982@lpu.in <srikant.12111982@lpu.in>
2025-05-11 20:28:04 +0530

    Diff aspects and output

--- Basics/Basics_02_DataTypes/Boolean.js
+++ Basics/Basics_02_DataTypes/Boolean.js
@@ -4,10 +4,11 @@ let bool1=true;
 let bool2=false;
 let bool3=Boolean(1);
 let bool4=Boolean(0);
-let bool5=Boolean('Hello')//if you pass astring its true
-let bool6=Boolean('')//if you pass an empty string its false
+let bool5=Boolean('Hello')  //if you pass astring its true
+let bool6=Boolean('')       //if you pass an empty string its false
+let bool7=Boolean()         // this is also false
 
-console.table({bool1,bool2,bool3,bool4,bool5,bool6})
+console.table({bool1,bool2,bool3,bool4,bool5,bool6,bool7})
 
 
 
